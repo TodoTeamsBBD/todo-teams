a5e549b45e7e5af2a66eb5d6316aab19a522a10e
RonJ <Ron.Joseph@bbd.co.za>
2025-06-03 22:45:24 +0200

    Slight sql change

--- flyway/V1__InitialSetup.sql
+++ flyway/V1__InitialSetup.sql
@@ -1,5 +1,5 @@
 CREATE TABLE "users" (
-  "id" int PRIMARY KEY NOT NULL,
+  "id" uuid PRIMARY KEY NOT NULL,
   "username" varchar(30) NOT NULL,
   "email" varchar(254) UNIQUE NOT NULL,
   "password_hash" varchar(255) NOT NULL,
@@ -20,7 +20,7 @@ CREATE TABLE "roles" (
 
 CREATE TABLE "user_roles" (
   "id" int PRIMARY KEY NOT NULL,
-  "user_id" int NOT NULL,
+  "user_id" uuid NOT NULL,
   "team_id" int,
   "role_id" int NOT NULL
 );
@@ -29,8 +29,8 @@ CREATE TABLE "todos" (
   "id" int PRIMARY KEY NOT NULL,
   "title" varchar(100) NOT NULL,
   "description" varchar(500),
-  "created_by" int NOT NULL,
-  "assigned_to" int NOT NULL,
+  "created_by" uuid NOT NULL,
+  "assigned_to" uuid NOT NULL,
   "team_id" int NOT NULL,
   "created_at" timestamp NOT NULL,
   "completed_at" timestamp
@@ -47,4 +47,3 @@ ALTER TABLE "todos" ADD FOREIGN KEY ("created_by") REFERENCES "users" ("id");
 ALTER TABLE "todos" ADD FOREIGN KEY ("assigned_to") REFERENCES "users" ("id");
 
 ALTER TABLE "todos" ADD FOREIGN KEY ("team_id") REFERENCES "teams" ("id");
-s
