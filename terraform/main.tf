819c5673ef2d9896be083062a75de0176f9f1ece
Keith Hughes <keith.hughes@bbd.co.za>
2025-06-08 17:51:12 +0200

    remove manually create resources

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,21 +1,3 @@
-resource "aws_s3_bucket" "tf_backend" {
-  bucket = "team7-terraform-state"
-
-  lifecycle {
-  }
-}
-
-resource "aws_dynamodb_table" "tf_lock" {
-  name         = "terraform-locks"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
-
 terraform {
   backend "s3" {
     bucket         = "team7-terraform-state"
