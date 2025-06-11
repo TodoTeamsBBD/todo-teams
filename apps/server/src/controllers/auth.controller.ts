bfde76016272f0f865a398134d835c891f5b4400
Keith Hughes <keith.hughes@bbd.co.za>
2025-06-11 12:13:33 +0200

    update cookie options in api

--- apps/server/src/controllers/auth.controller.ts
+++ apps/server/src/controllers/auth.controller.ts
@@ -45,7 +45,7 @@ export const signup = async (req: Request, res: Response) => {
     res.cookie('access_token', token, {
         httpOnly: true,
         secure: true,
-        sameSite: 'strict',
+        sameSite: 'none',
         maxAge: 24 * 60 * 60 * 1000,
     });
 
@@ -189,7 +189,7 @@ export const login = async (req: Request, res: Response) => {
     res.cookie('access_token', token, {
         httpOnly: true,
         secure: true,
-        sameSite: 'strict',
+        sameSite: 'none',
         maxAge: 24 * 60 * 60 * 1000,
     });
 
@@ -241,7 +241,7 @@ export const login2FA = async (req: Request, res: Response) => {
     res.cookie('access_token', token, {
         httpOnly: true,
         secure: true,
-        sameSite: 'strict',
+        sameSite: 'none',
         maxAge: 24 * 60 * 60 * 1000,
     });
 
@@ -256,7 +256,7 @@ export const logout = (_: Request, res: Response) => {
     res.clearCookie('access_token', {
         httpOnly: true,
         secure: true,
-        sameSite: 'strict',
+        sameSite: 'none',
     });
     return res.send('Logged out successfully');
 };
